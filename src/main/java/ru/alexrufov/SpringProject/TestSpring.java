8ab66713355ba9021b880fe48790078d7cfa6066
Fatal <Dragonishe@yandex.ru>
2020-05-04 14:13:42 +0300

    Test

--- src/main/java/ru/alexrufov/SpringProject/TestSpring.java
+++ src/main/java/ru/alexrufov/SpringProject/TestSpring.java
@@ -6,9 +6,7 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music musicBean = context.getBean("musicBean", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
